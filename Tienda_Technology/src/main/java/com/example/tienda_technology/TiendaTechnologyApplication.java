cb6961eca4e31a892b407b7e10cf049c69c0ce90
Kleiz28 <U23234993@utp.edu.pe>
2025-09-20 01:33:17 -0500

    feat: Demo de la Tienda Technology

---
+++ Tienda_Technology/src/main/java/com/example/tienda_technology/TiendaTechnologyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tienda_technology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTechnologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaTechnologyApplication.class, args);
+    }
+
+}
